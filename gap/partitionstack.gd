7fa190e03529fd5388af9eae40873cd559e91312
Wilf Wilson <wilf@wilf-wilson.net>
2019-02-11 15:56:46 +0100

    Expand partitionstack.gd documentation

--- gap/partitionstack.gd
+++ gap/partitionstack.gd
@@ -1,20 +1,21 @@
-#! @Chapter Ordered Partition Stack
+#! @Chapter Ordered partition stack
 #!
-#! An Ordered Partition Stack is an Ordered Partition which supports
+#! An <E>ordered partition stack</E> is an ordered partition which supports
 #! splitting a cell, and then later undoing a change, reverting the
-#! partition back to an earlier start.
+#! partition back to an earlier state.
 
 
 #! @Section API
 #!
 #! @Description
-#! Constructor for partition stacks
+#! Constructor for partition stacks.
+#!
 #! @Arguments
-#! @Returns a partition stack
+#! @Returns a partition stack.
 DeclareGlobalFunction("PartitionStack");
 
 #! @Description
-#! Category of partition stacks
+#! Category of partition stacks.
 DeclareCategory("IsPartitionStack", IsObject);
 BindGlobal( "PartitionStackFamily", NewFamily("PartitionStackFamily") );
 
@@ -24,81 +25,131 @@ BindGlobal( "PartitionStackType", NewType(PartitionStackFamily, IsPartitionStack
 BindGlobal( "PartitionStackTypeMutable", NewType(PartitionStackFamily,
                                         IsPartitionStackRep and IsMutable));
 
-#! The number of points in the partition
+#! @Description
+#! Return the number of points on which the partition stack <A>PS</A> is
+#! defined.
+#!
+#! @Arguments PS
+#! @Returns a positive integer.
 DeclareOperation("PS_Points", [IsPartitionStack]);
 
-#! The number of cells in the partition
+#! @Description
+#! The number of cells in the current partition state of the partition stack
+#! <A>PS</A>.
+#!
+#! @Arguments PS
+#! @Returns a positive integer.
 DeclareOperation("PS_Cells", [IsPartitionStack]);
 
-#! Returns if all cells are of size 1
+#! @Description
+#! Return <K>true</K> if all the cells of the current partition state of the
+#! partition stack <A>PS</A> have size 1.
+#!
+#! @Arguments PS
+#! @Returns <K>true</K> or <K>false</K>.
 DeclareOperation("PS_Fixed", [IsPartitionStack]);
 
-#! Return the current partition state as a list of sets
+#! @Description
+#! Return the current partition state of the partition stack <A>PS</A> as a
+#! list of sets.
+#!
+#! @Arguments PS
+#! @Returns a list of lists of positive integers.
 DeclareOperation("PS_AsPartition", [IsPartitionStack]);
 
 #! @Description
-#! The size of cell i
+#! The size of cell <A>i</A> in the current partition state of the partition
+#! stack <A>PS</A>.
+#! This requires that <A>i</A> is contained in <C>[1..PS_Cells(<A>PS</A>)]</C>.
+#!
 #! @Arguments PS, i
+#! @Returns a positive integer.
 DeclareOperation("PS_CellLen", [IsPartitionStack, IsPosInt]);
 
 #! @Description
-#! Return an immutable list containing the elements of cell i
+#! Return an immutable list containing the elements of cell <A>i</A> in
+#! the current partition state of the partition stack <A>PS</A>.
+#! This requires that <A>i</A> is contained in <C>[1..PS_Cells(<A>PS</A>)]</C>.
+#!
 #! @Arguments PS, i
+#! @Returns an immutable list of positive integers.
 DeclareOperation("PS_CellSlice", [IsPartitionStack, IsPosInt]);
 
 #! @Description
-#! Return a list of the cells of size 1, in the order they were created
+#! Return a list of the 1-element cells of the current state of the partition
+#! stack <A>PS</A>, in the order in which the cells came to have size 1.
+#!
 #! @Arguments PS
+#! @Returns a list of 1-element lists of positive integers.
 DeclareOperation("PS_FixedCells", [IsPartitionStack]);
 
 #! @Description
-#! Return a list of points in cells of size 1, in the order they were created
+#! Return a list of points in 1-element cells of the partition stack <A>PS</A>,
+#! in the order in which the cells came to have size 1. In other words, return
+#! <C>Concatenation(PS_FixedCells(<A>PS</A>))</C>.
+#!
 #! @Arguments PS
+#! @Returns a list of positive integers.
 DeclareOperation("PS_FixedPoints", [IsPartitionStack]);
 
 #! @Description
-#! Return the cell which contains value i
+#! Return the index of the cell containing the value <A>i</A> in the current
+#! partition state of the partition stack <A>PS</A>.
+#! This requires that <A>i</A> is contained in <C>[1..PS_Points(<A>PS</A>)]</C>.
+#!
 #! @Arguments PS, i
+#! @Returns a positive integer.
 DeclareOperation("PS_CellOfPoint", [IsPartitionStack, IsPosInt]);
 
 #! @Description
-#! Revert the state of the partition stack to when there were i cells.
-#! This requires that i is less than PS_Cells(PS)
+#! Revert the state of the partition stack <A>PS</A> to when there were
+#! <A>i</A> cells.
+#! This requires that <A>i</A> is contained in <C>[1..PS_Cells(<A>PS</A>)]</C>.
+#!
 #! @Arguments PS, i
+#! @Returns nothing.
 DeclareOperation("PS_RevertToCellCount", [IsPartitionStack, IsPosInt]);
 
+#TODO: what is the different between ByFunction and ByUnorderedFunction?
 #! @Description
-#! Split cell i of a partition stack. The values in the cell are split
-#! so that values in the cell with different images under f are put into
+#! Split cell <A>i</A> of the current partition state of the partition stack
+#! <A>PS</A> according to the function <A>f</A>. The values in the cell are
+#! split so that those with different images under <A>f</A> are put into
 #! different cells.
+#! The second argument <A>t</A> should be a tracer.
 #!
-#! @Arguments PS, i, f
+#! @Arguments PS, t, i, f
+#! @Returns <K>true</K> or <K>false</K>.
 DeclareOperation("PS_SplitCellByFunction", [IsPartitionStack, IsTracer, IsPosInt, IsFunction]);
 
 #! @Description
-#! Apply SplitCellByFunction to every cell in PS
+#! Apply <C>PS_SplitCellByFunction</C> to every active cell in the partition
+#! stack <A>PS</A>.
 #!
-#! @Arguments PS, f
-DeclareOperation("PS_SplitCellsByUnorderedFunction", [IsPartitionStack, IsTracer, IsFunction]);
+#! @Arguments PS, t, f
+#! @Returns <K>true</K> or <K>false</K>.
+DeclareOperation("PS_SplitCellsByFunction", [IsPartitionStack, IsTracer, IsFunction]);
 
 #! @Description
-#! Split cell i of a partition stack. The values in the cell are split
-#! so that values in the cell with different images under f are put into
+#! Split cell <A>i</A> of the current partition state of the partition stack
+#! <A>PS</A> according to the function <A>f</A>. The values in the cell are
+#! split so that those with different images under <A>f</A> are put into
 #! different cells.
+#! The second argument <A>t</A> should be a tracer.
 #!
-#! @Arguments PS, i, f
+#! @Arguments PS, t, i, f
 DeclareOperation("PS_SplitCellByUnorderedFunction", [IsPartitionStack, IsTracer, IsPosInt, IsFunction]);
 
 #! @Description
-#! Apply SplitCellByFunction to every cell in PS
+#! Apply <C>PS_SplitCellByUnorderedFunction</C> to every active cell in the
+#! partition stack <A>PS</A>.
 #!
-#! @Arguments PS, f
-DeclareOperation("PS_SplitCellsByFunction", [IsPartitionStack, IsTracer, IsFunction]);
+#! @Arguments PS, t, f
+DeclareOperation("PS_SplitCellsByUnorderedFunction", [IsPartitionStack, IsTracer, IsFunction]);
 
 DeclareOperation("PS_UNSAFE_CellSlice", [IsPartitionStack, IsPosInt]);
 
 DeclareOperation("PS_UNSAFE_FixupCell", [IsPartitionStack, IsPosInt]);
 
 DeclareInfoClass("InfoPartitionStack");
-
 SetInfoLevel(InfoPartitionStack, 1);
